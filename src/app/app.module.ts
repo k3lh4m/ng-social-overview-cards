c86c2d40444ed71e1c2209af17dcb3a453156c99
samkelham <sam@kelham.co>
2018-02-13 22:35:42 +0700

    feat(api-service): update app with HttpClient module to replace Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 import {ApiService} from './services/api.service';
-import { HttpClientModule } from '@angular/common/http';
-import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+
 import {SocialOverviewContainerComponent} from './social-overview/social-overview-container.component';
 import {SocialOverviewImageComponent} from './social-overview/social-overview-single/social-overview-image/social-overview-image.component';
-import { SocialOverviewSingleComponent } from './social-overview/social-overview-single/social-overview-single.component';
-import { SocialOverviewBioComponent } from './social-overview/social-overview-single/social-overview-bio/social-overview-bio.component';
-import { SocialOverviewSocialStatsComponent } from './social-overview/social-overview-single/social-overview-social-stats/social-overview-social-stats.component';
-import { CreateFormComponent } from './social-overview/user-forms/create-form/create-form.component';
+import {SocialOverviewSingleComponent} from './social-overview/social-overview-single/social-overview-single.component';
+import {SocialOverviewBioComponent} from './social-overview/social-overview-single/social-overview-bio/social-overview-bio.component';
+import {SocialOverviewSocialStatsComponent} from './social-overview/social-overview-single/social-overview-social-stats/social-overview-social-stats.component';
+import {CreateFormComponent} from './social-overview/user-forms/create-form/create-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { SocialOverviewEditUserComponent } from './social-overview/shared/social-overview-edit-user/social-overview-edit-user.component';
-import { EditFormComponent } from './social-overview/user-forms/edit-form/edit-form.component';
+import {SocialOverviewEditUserComponent} from './social-overview/shared/social-overview-edit-user/social-overview-edit-user.component';
+import {EditFormComponent} from './social-overview/user-forms/edit-form/edit-form.component';
 import {SocialOverviewDeleteUserComponent} from "./social-overview/shared/social-overview-delete-user/social-overview-delete-user.component";
-
 
 @NgModule({
   declarations: [
@@ -32,11 +31,11 @@ import {SocialOverviewDeleteUserComponent} from "./social-overview/shared/social
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpModule,
     FormsModule,
     ReactiveFormsModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,57 +1,43 @@
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {ApiSettings} from '../constants/api.const';
 import {IUser} from '../interfaces/social-overview-users.interface';
 import {Observable} from "rxjs";
 import 'rxjs/Rx';
 
+const httpHeaders = {
+  header: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable()
 export class ApiService {
-  private _http: Http;
+  private _http: HttpClient;
 
-  constructor(http: Http) {
-    this._http = http;
+  constructor(httpClient: HttpClient) {
+    this._http = httpClient;
   }
 
   public getRecord(url: string) {
-    return this._http.get(url)
-      .map((response) => {
-        return response.json();
-      });
+    return this._http.get(url);
   }
 
   public getRecordById(id: string): Observable<IUser> {
-    return this._http.get(ApiSettings.apiBasepath + 'users/' + id)
-      .map(this.extractData)
+    return this._http.get(ApiSettings.apiBasepath + 'users/' + id);
   }
 
   public updateRecord(body: IUser, id: number): Observable<IUser> {
     let bodyObject = JSON.stringify(body);
-    let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({headers: headers});
 
-    return this._http.put(ApiSettings.apiBasepath + 'users/' + id, bodyObject, options)
-      .map((res: Response) => res.json())
-      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+    return this._http.put(ApiSettings.apiBasepath + 'users/' + id, bodyObject, httpHeaders)
   }
 
   public deleteRecord(id: number): Observable<IUser> {
-    return this._http.delete(ApiSettings.apiBasepath + 'users/' + id).map((res: Response) => res.json()) // ...and calling .json() on the response to return data
-      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    return this._http.delete(ApiSettings.apiBasepath + 'users/' + id);
   }
 
   public addRecord(body: IUser): Observable<IUser> {
     let bodyObject = JSON.stringify(body);
-    let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({headers: headers});
 
-    return this._http.post(ApiSettings.apiBasepath + 'users/', bodyObject, options)
-      .map((res: Response) => res.json())
-      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
-  }
-
-  private extractData(res: Response) {
-    let responseBody = res.json();
-    return responseBody;
+    return this._http.post(ApiSettings.apiBasepath + 'users/', bodyObject, httpHeaders);
   }
 }
