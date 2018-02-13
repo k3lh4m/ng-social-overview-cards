77a58242b4e04fb49b4967efd449f72591f0089d
samkelham <sam@kelham.co>
2018-02-13 21:53:14 +0700

    fix(api-service): import rxjs

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,7 +3,7 @@ import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {ApiSettings} from '../constants/api.const';
 import {IUser} from '../interfaces/social-overview-users.interface';
 import {Observable} from "rxjs";
-import 'rxjs/add/operator/map'
+import 'rxjs/Rx';
 
 @Injectable()
 export class ApiService {
@@ -16,8 +16,8 @@ export class ApiService {
   public getRecord(url: string) {
     return this._http.get(url)
       .map((response) => {
-      return response.json();
-    });
+        return response.json();
+      });
   }
 
   public getRecordById(id: string): Observable<IUser> {
@@ -27,27 +27,27 @@ export class ApiService {
 
   public updateRecord(body: IUser, id: number): Observable<IUser> {
     let bodyObject = JSON.stringify(body);
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
 
     return this._http.put(ApiSettings.apiBasepath + 'users/' + id, bodyObject, options)
-      .map((res:Response) => res.json())
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
   }
 
   public deleteRecord(id: number): Observable<IUser> {
-    return this._http.delete(ApiSettings.apiBasepath + 'users/' + id).map((res:Response) => res.json()) // ...and calling .json() on the response to return data
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    return this._http.delete(ApiSettings.apiBasepath + 'users/' + id).map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
 
   public addRecord(body: IUser): Observable<IUser> {
     let bodyObject = JSON.stringify(body);
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
 
-    return this._http.post( ApiSettings.apiBasepath + 'users/', bodyObject, options)
-      .map((res:Response) => res.json())
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+    return this._http.post(ApiSettings.apiBasepath + 'users/', bodyObject, options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
   }
 
   private extractData(res: Response) {
