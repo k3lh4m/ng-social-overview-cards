bfe81a444aba6c9125ce395ed02bae35d83e5070
samkelham <sam@kelham.co>
2018-02-17 19:53:04 +0700

    feat(validate-url): add directive which validates a a string against a url regex

---
+++ src/app/directives/validate-url/validate-url.directive.ts
@@ -0,0 +1,29 @@
+import {Directive, forwardRef} from '@angular/core';
+import {NG_VALIDATORS, AbstractControl} from "@angular/forms";
+
+@Directive({
+  selector: '[appValidateUrl][ngModel]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: forwardRef(() => ValidateUrlDirective ), multi: true }
+  ]
+})
+export class ValidateUrlDirective {
+
+  validate(control: AbstractControl): {[validator: string]: string} {
+    if (!control.value) { // the [required] validator will check presence, not us
+      return null;
+    }
+
+    const expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    const regex = new RegExp(expression);
+    const value = control.value.trim();
+
+    if (value.match(regex)) {
+      return null;
+    }
+
+
+    return { validateUrl: 'Not valid url' };
+  }
+
+}
