ae35dd736ea171dfb0ddbb0c1c0881cfd723c01b
samkelham <sam@kelham.co>
2018-02-17 20:23:22 +0700

    feat(form-text-input): add component that represents a text input

---
+++ src/app/social-overview/user-forms/form-components/form-text-input/form-text-input.component.ts
@@ -0,0 +1,36 @@
+import {Component, Input, Optional, Inject, ViewChild, ViewEncapsulation} from '@angular/core';
+
+import {
+  NgModel,
+  NG_VALUE_ACCESSOR,
+  NG_VALIDATORS,
+  NG_ASYNC_VALIDATORS,
+} from '@angular/forms';
+import {ElementBase} from "../../element-base/element-base.class";
+
+@Component({
+  selector: 'app-form-text-input',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: 'form-text-input.component.html',
+  styleUrls: ['form-text-input.component.scss'],
+  providers: [
+    {provide: NG_VALUE_ACCESSOR, useExisting: FormTextInputComponent, multi: true}
+  ],
+})
+export class FormTextInputComponent extends ElementBase<string>{
+  @Input() public label: string;
+  @Input() public placeholder: string;
+
+  @ViewChild(NgModel) model: NgModel;
+
+  public identifier = `form-text-${identifier++}`;
+
+  constructor(
+    @Optional() @Inject(NG_VALIDATORS) private validators: Array<any>,
+    @Optional() @Inject(NG_ASYNC_VALIDATORS) private asyncValidators: Array<any>,
+  ) {
+    super(validators, asyncValidators);
+  }
+}
+
+let identifier = 0;
