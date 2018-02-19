df0ef87377fb1c537d8e718207ef7b05dea21cd2
samkelham <sam@kelham.co>
2018-02-19 17:06:29 +0700

    chore(enviroment): add firebase config to env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAm8Jbwgvy9RCtnt-Tk1mdpsJtL492D-nA",
+    authDomain: "social-overview.firebaseapp.com",
+    databaseURL: "https://social-overview.firebaseio.com",
+    projectId: "social-overview",
+    storageBucket: "social-overview.appspot.com",
+    messagingSenderId: "704797445720"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAm8Jbwgvy9RCtnt-Tk1mdpsJtL492D-nA",
+    authDomain: "social-overview.firebaseapp.com",
+    databaseURL: "https://social-overview.firebaseio.com",
+    projectId: "social-overview",
+    storageBucket: "social-overview.appspot.com",
+    messagingSenderId: "704797445720"
+  }
 };
